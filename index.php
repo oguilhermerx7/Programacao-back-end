a222d1c669eafd11148b2591b883f61ea44d9078
Guilherme Schmidt <129059738+oguilhermerx7@users.noreply.github.com>
2024-02-28 11:58:09 -0300

    Arquivo de texto com variaveis

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variavel</title>
+</head>
+<style>
+    body{
+        background-color: red;
+        align-items: center;
+        font-family: Arial, Helvetica, sans-serif;
+    }
+    </style>
+<body>    
+    <?php
+    $nome = "Guilherme";
+    $sobrenome = "Schmidt";
+    $idade = 16;
+    $peso = 97.5;
+    $altura = 1.81;
+
+    echo "Seus dados são $nome", "<br>";
+    echo "com o sobrenome $sobrenome","<br>";
+    echo "de idade $idade","<br>";
+    echo "com o peso de $peso","<br>";
+    echo "e altura de $altura","<br>";
+    ?>
+</body>
+</html>
